328dab46a877b0e0242ac3523c80ac8e0b4feb6d
eduardo_frois <efroisdrumond@gmail.com>
2024-08-05 18:41:35 -0300

    feat: first commit

---
+++ Models/Contatos.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Contatos
+    {
+        [Key]
+        public int codigo { get; set; }
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public string numero { get; set; }
+        public string cpf { get; set; }
+        public string email { get; set; }
+    }
+}
